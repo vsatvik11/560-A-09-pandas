4d826593751370f43c18cd843517c1a04c48fbbe
SatvikV123 <107970663+SatvikV123@users.noreply.github.com>
2025-11-11 19:32:34 -0500

    Removing the roster variable

--- roster.py
+++ roster.py
@@ -2,8 +2,8 @@
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 roster = ["Dixon", "Young", "Denis"]
-player = {"Last Name": roster,
-          "First Name": ["Armando", "RJ", "Elliot"],
+player = {"Last Name": ["Dixon", "Young", "Denis"],
+          "First Name": ["Derek", "Jaydon", "Denis"],
           "height": [83,72,73], 
           "weight": [240,180,180]}
 data = pd.DataFrame(roster)
