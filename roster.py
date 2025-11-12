1c0c370e2b0bb126d88528d1fe50100c64be3f9e
SatvikV123 <107970663+SatvikV123@users.noreply.github.com>
2025-11-11 19:29:36 -0500

    Last Name

--- roster.py
+++ roster.py
@@ -2,5 +2,6 @@
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 roster = ["Dixon", "Young", "Denis"]
+player = {"Last Name": roster}
 data = pd.DataFrame(roster)
 print(data)
